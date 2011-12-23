octo47/ceph

commit fa722de6708d3e92037df6289cc29ece12c8ea66

osd: move queue checks into enqueue_op, kill _handle_ helpers
This simplifies things, and renames the checks to make it clear that we are
doing validation checks only, with no side-effects allowed.

Also move some checks into the parent handle_op() to further simplify the
(re)queue checks.

Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

src/osd/OSD.cc
@@ -5152,6 +5152,9 @@ void OSD::handle_op(MOSDOp *op)
     return;
   }
 
+  // we don't need encoded payload anymore
+  op->clear_payload();
+
   // require same or newer map
   if (!require_same_or_newer_map(op, op->get_map_epoch()))
     return;
@@ -5168,6 +5171,27 @@ void OSD::handle_op(MOSDOp *op)
   if (osdmap->is_blacklisted(op->get_source_addr())) {
     dout(4) << "handle_op " << op->get_source_addr() << " is blacklisted" << dendl;
     reply_op_error(op, -EBLACKLISTED);
+    return;
+  }
+
+  // full?
+  if (osdmap->test_flag(CEPH_OSDMAP_FULL) &&
+      !op->get_source().is_mds()) {  // FIXME: we'll exclude mds writes for now.
+    reply_op_error(op, -ENOSPC);
+    return;
+  }
+
+  // invalid?
+  if (op->get_snapid() != CEPH_NOSNAP) {
+    reply_op_error(op, -EINVAL);
+    return;
+  }
+
+  // too big?
+  if (g_conf->osd_max_write_size &&
+      op->get_data_len() > g_conf->osd_max_write_size << 20) {
+    // journal can't hold commit!
+    reply_op_error(op, -OSD_WRITETOOBIG);
     return;
   }
 
@@ -5198,7 +5222,7 @@ void OSD::handle_op(MOSDOp *op)
   }
 
   pg->get();
-  _handle_op(pg, op);
+  enqueue_op(pg, op);
   pg->unlock();
   pg->put();
 }
@@ -5234,77 +5258,6 @@ bool OSD::op_has_sufficient_caps(PG *pg, MOSDOp *op)
   return true;
 }
 
-/*
- * queue an operation, or discard it.  avoid side-effects or any "real" work.
- */
-void OSD::_handle_op(PG *pg, MOSDOp *op)
-{
-  dout(10) << *pg << " _handle_op " << op << " " << *op << dendl;
-  assert(pg->is_locked());
-
-  if (!op_has_sufficient_caps(pg, op)) {
-    reply_op_error(op, -EPERM);
-    return;
-  }
-
-  // we don't need encoded payload anymore
-  op->clear_payload();
- 
-  if (op->may_write()) {
-    // misdirected?
-    if (!pg->is_primary() ||
-	!pg->same_for_modify_since(op->get_map_epoch())) {
-      handle_misdirected_op(pg, op);
-      return;
-    }
-
-    // full?
-    if (osdmap->test_flag(CEPH_OSDMAP_FULL) &&
-	!op->get_source().is_mds()) {  // FIXME: we'll exclude mds writes for now.
-      reply_op_error(op, -ENOSPC);
-      return;
-    }
-
-    // invalid?
-    if (op->get_snapid() != CEPH_NOSNAP) {
-      reply_op_error(op, -EINVAL);
-      return;
-    }
-
-    // too big?
-    if (g_conf->osd_max_write_size &&
-        op->get_data_len() > g_conf->osd_max_write_size << 20) {
-      // journal can't hold commit!
-      reply_op_error(op, -OSD_WRITETOOBIG);
-      return;
-    }
-  } else {
-    // misdirected?
-    if (!pg->same_for_read_since(op->get_map_epoch())) {
-      handle_misdirected_op(pg, op);
-      return;
-    }
-  }
-
-  // pg must be active.
-  if (!pg->is_active()) {
-    dout(7) << *pg << " not active (yet)" << dendl;
-    pg->waiting_for_active.push_back(op);
-    return;
-  }
-  if (pg->is_replay()) {
-    if (op->get_version().version > 0) {
-      dout(7) << *pg << " queueing replay at " << op->get_version()
-	      << " for " << *op << dendl;
-      pg->replay_queue[op->get_version()] = op;
-      return;
-    }
-  }
-
-  enqueue_op(pg, op);         
-}
-
-
 void OSD::handle_sub_op(MOSDSubOp *op)
 {
   dout(10) << "handle_sub_op " << *op << " epoch " << op->map_epoch << dendl;
@@ -5337,30 +5290,9 @@ void OSD::handle_sub_op(MOSDSubOp *op)
     return;
   }
   pg->get();
-  _handle_sub_op(pg, op);
+  enqueue_op(pg, op);
   pg->unlock();
   pg->put();
-}
-
-/*
- * queue an operation, or discard it.  avoid side-effects or any "real" work.
- */
-void OSD::_handle_sub_op(PG *pg, MOSDSubOp *op)
-{
-  dout(10) << *pg << " _handle_sub_op " << op << " " << *op << dendl;
-  assert(pg->is_locked());
-
-  // same pg?
-  //  if pg changes _at all_, we reset and repeer!
-  if (op->map_epoch < pg->info.history.same_interval_since) {
-    dout(10) << "handle_sub_op pg changed " << pg->info.history
-	     << " after " << op->map_epoch 
-	     << ", dropping" << dendl;
-    op->put();
-    return;
-  }
-
-  enqueue_op(pg, op);     // queue for worker threads
 }
 
 void OSD::handle_sub_op_reply(MOSDSubOpReply *op)
@@ -5393,21 +5325,74 @@ void OSD::handle_sub_op_reply(MOSDSubOpReply *op)
     return;
   }
   pg->get();
-  _handle_sub_op_reply(pg, op);
+  enqueue_op(pg, op);
   pg->unlock();
   pg->put();
 }
 
 /*
- * queue an operation, or discard it.  avoid side-effects or any "real" work.
+ * discard operation, or return true.  no side-effects.
  */
-void OSD::_handle_sub_op_reply(PG *pg, MOSDSubOpReply *op)
+bool OSD::op_is_queueable(PG *pg, MOSDOp *op)
 {
-  dout(10) << *pg << " _handle_sub_op_reply " << op << " " << *op << dendl;
   assert(pg->is_locked());
-  enqueue_op(pg, op);     // queue for worker threads
+
+  if (!op_has_sufficient_caps(pg, op)) {
+    reply_op_error(op, -EPERM);
+    return false;
+  }
+
+  // misdirected?
+  if (op->may_write()) {
+    if (!pg->is_primary() ||
+	!pg->same_for_modify_since(op->get_map_epoch())) {
+      handle_misdirected_op(pg, op);
+      return false;
+    }
+  } else {
+    if (!pg->same_for_read_since(op->get_map_epoch())) {
+      handle_misdirected_op(pg, op);
+      return false;
+    }
+  }
+
+  if (!pg->is_active()) {
+    dout(7) << *pg << " not active (yet)" << dendl;
+    pg->waiting_for_active.push_back(op);
+    return false;
+  }
+
+  if (pg->is_replay()) {
+    if (op->get_version().version > 0) {
+      dout(7) << *pg << " queueing replay at " << op->get_version()
+	      << " for " << *op << dendl;
+      pg->replay_queue[op->get_version()] = op;
+      return false;
+    }
+  }
+
+  return true;
 }
 
+/*
+ * discard operation, or return true.  no side-effects.
+ */
+bool OSD::subop_is_queueable(PG *pg, MOSDSubOp *op)
+{
+  assert(pg->is_locked());
+
+  // same pg?
+  //  if pg changes _at all_, we reset and repeer!
+  if (op->map_epoch < pg->info.history.same_interval_since) {
+    dout(10) << "handle_sub_op pg changed " << pg->info.history
+	     << " after " << op->map_epoch 
+	     << ", dropping" << dendl;
+    op->put();
+    return false;
+  }
+
+  return true;
+}
 
 /*
  * enqueue called with osd_lock held
@@ -5416,6 +5401,26 @@ void OSD::enqueue_op(PG *pg, Message *op)
 {
   dout(15) << *pg << " enqueue_op " << op << " " << *op << dendl;
   assert(pg->is_locked());
+
+  switch (op->get_type()) {
+  case CEPH_MSG_OSD_OP:
+    if (!op_is_queueable(pg, (MOSDOp*)op))
+      return;
+    break;
+
+  case MSG_OSD_SUBOP:
+    if (!subop_is_queueable(pg, (MOSDSubOp*)op))
+      return;
+    break;
+
+  case MSG_OSD_SUBOPREPLY:
+    // don't care.
+    break;
+
+  default:
+    assert(0 == "enqueued an illegal message type");
+  }
+
   // add to pg's op_queue
   pg->op_queue.push_back(op);
   pending_ops++;
@@ -5448,20 +5453,7 @@ void OSD::requeue_ops(PG *pg, list<Message*>& ls)
   while (!q.empty()) {
     Message *op = q.front();
     q.pop_front();
-
-    switch (op->get_type()) {
-    case CEPH_MSG_OSD_OP:
-      _handle_op(pg, (MOSDOp*)op);
-      break;
-    case MSG_OSD_SUBOP:
-      _handle_sub_op(pg, (MOSDSubOp*)op);
-      break;
-    case MSG_OSD_SUBOPREPLY:
-      _handle_sub_op_reply(pg, (MOSDSubOpReply*)op);
-      break;
-    default:
-      assert(0 == "requeued an illegal type");
-    }
+    enqueue_op(pg, op);
   }
 
   // put orig queue contents back in line, after the stuff we requeued.

src/osd/OSD.h
@@ -1094,14 +1094,8 @@ public:
 private:
   /// check if op has sufficient caps
   bool op_has_sufficient_caps(PG *pg, class MOSDOp *m);
-
-  /*
-   * these locked helpers assume pg is locked, and will do fina
-   * checks before enqueuing the given operation.
-   */
-  void _handle_op(PG *pg, class MOSDOp *m);
-  void _handle_sub_op(PG *pg, class MOSDSubOp *m);
-  void _handle_sub_op_reply(PG *pg, class MOSDSubOpReply *m);
+  bool op_is_queueable(PG *pg, class MOSDOp *m);
+  bool subop_is_queueable(PG *pg, class MOSDSubOp *m);
 
 public:
   void force_remount();

commit 99dff2bcf72752686a6729895a9de5588ab9e6ed

OSD: Check that we're supposed to be getting a PG before waitlisting requests.
This was broken in fa722de6708d3e92037df6289cc29ece12c8ea66. Fix it by
adding a new check_op_mapping() function and calling it when we don't
have the PG for a request.
Rework handle_misdirected_op slightly so it doesn't require us to have
a PG object for misdirected ops, and fix the documentation for
op_is_queueable() to not be a gigantic lie.

Signed-off-by: Greg Farnum <gregory.farnum@dreamhost.com>

src/osd/OSD.cc
@@ -5107,18 +5107,52 @@ void OSD::reply_op_error(MOSDOp *op, int err, eversion_t v)
 
 void OSD::handle_misdirected_op(PG *pg, MOSDOp *op)
 {
-  if (op->get_map_epoch() < pg->info.history.same_primary_since) {
-    dout(7) << *pg << " changed after " << op->get_map_epoch() << ", dropping" << dendl;
-    op->put();
+  if (pg) {
+    if (op->get_map_epoch() < pg->info.history.same_primary_since) {
+      dout(7) << *pg << " changed after " << op->get_map_epoch() << ", dropping" << dendl;
+      op->put();
+      return;
+    } else {
+      dout(7) << *pg << " misdirected op in " << op->get_map_epoch() << dendl;
+      clog.warn() << op->get_source_inst() << " misdirected "
+          << op->get_reqid() << " " << pg->info.pgid << " to osd." << whoami
+          << " not " << pg->acting
+          << " in e" << op->get_map_epoch() << "/" << osdmap->get_epoch()
+          << "\n";
+    }
   } else {
-    dout(7) << *pg << " misdirected op in " << op->get_map_epoch() << dendl;
+    dout(7) << "got misdirected op from " << op->get_source_inst()
+            << " for pgid " << op->get_pg() << dendl;
     clog.warn() << op->get_source_inst() << " misdirected "
-		<< op->get_reqid() << " " << pg->info.pgid << " to osd." << whoami
-		<< " not " << pg->acting
-		<< " in e" << op->get_map_epoch() << "/" << osdmap->get_epoch()
-		<< "\n";
-    reply_op_error(op, -ENXIO);
+                << op->get_reqid() << " " << pg->info.pgid
+                << "to osd." << whoami
+                << " in e" << op->get_map_epoch() << "\n";
   }
+  reply_op_error(op, -ENXIO);
+}
+
+/*
+ * See if this OSD is a legitimate target for an op -- meaning it
+ * is an acting member of the PG. Checks for appropriateness (is primary
+ * for writes, etc) are out of scope here.
+ *
+ * @param was_valid_then If we are not now a legitimate target, this will
+ * be set to true if the op is directed properly for the given epoch.
+ * Otherwise it is false.
+ * @return true if we should keep the request, false otherwise.
+ */
+bool OSD::check_op_mapping(MOSDOp *op, bool *was_valid_then)
+{
+  *was_valid_then = false;
+  if (osdmap->get_pg_role(op->get_pg(), whoami) >= 0)
+    return true;
+
+  // okay, we aren't valid now; check send epoch
+  OSDMapRef send_map = get_map(op->get_map_epoch());
+  if (send_map->get_pg_role(op->get_pg(), whoami) >= 0) {
+    *was_valid_then = true;
+  }
+  return false;
 }
 
 void OSD::handle_op(MOSDOp *op)
@@ -5193,9 +5227,19 @@ void OSD::handle_op(MOSDOp *op)
   // get and lock *pg.
   PG *pg = _have_pg(pgid) ? _lookup_lock_pg(pgid) : NULL;
   if (!pg) {
-    dout(7) << "hit non-existent pg " << pgid 
-	    << ", waiting" << dendl;
-    waiting_for_pg[pgid].push_back(op);
+    dout(7) << "hit non-existent pg " << pgid << dendl;
+    bool was_valid_then;
+    if (check_op_mapping(op, &was_valid_then)) {
+      dout(7) << "we are valid target for op, waiting" << dendl;
+      waiting_for_pg[pgid].push_back(op);
+    } else if (!was_valid_then) {
+      dout(7) << "we are invalid target" << dendl;
+      handle_misdirected_op(NULL, op);
+    } else {
+      dout(7) << "dropping request; client will resend when they get new map"
+              << dendl;
+      op->put();
+    }
     return;
   }
 
@@ -5323,7 +5367,12 @@ bool OSD::op_is_discardable(MOSDOp *op)
 }
 
 /*
- * discard operation, or return true.  no side-effects.
+ * Determine if we can queue the op right now; if not this deals with it.
+ * If it's not queueable, we deal with it in one of a few ways:
+ * dropping the request, putting it into a wait list for later, or
+ * telling the sender that the request was misdirected.
+ *
+ * @return true if the op is queueable; false otherwise.
  */
 bool OSD::op_is_queueable(PG *pg, MOSDOp *op)
 {

src/osd/OSD.h
@@ -1072,6 +1072,7 @@ public:
   void reply_op_error(MOSDOp *op, int r);
   void reply_op_error(MOSDOp *op, int r, eversion_t v);
   void handle_misdirected_op(PG *pg, MOSDOp *op);
+  bool check_op_mapping(MOSDOp *op, bool *was_valid_then);
 
   void handle_rep_scrub(MOSDRepScrub *m);
   void handle_scrub(class MOSDScrub *m);